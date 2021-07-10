42f62340d2392631e2a9ac12fd86f5a22b8e1222
Frago9876543210 <al.volf.av@gmail.com>
2021-07-10 19:54:00 +0300

    Integrate symbols library with modloader

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <modloader/loader.h>
+#include <symbols.h>
+#include "elf_helper.h"
 
 using namespace modloader;
 
 __attribute__((constructor))
 static void modloader_init() {
+    set_base_offset(ElfHelper::getCurrentModule());
     ModLoader::addLibSearchDir("mods");
     ModLoader::loadModsFromDirectory("mods");
 }
---
+++ symbols/include/symbols.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void set_base_offset(void *);
+
+#ifdef __cplusplus
+}
+#endif
